4d32b12a98477d0ee15b4784e3c02bb66e4822c5
rywags <rywa2447@colorado.edu>
2024-10-01 15:25:01 -0600

    Changed html to md

--- dem.h
+++ dem.h
@@ -1,9 +1,15 @@
 #ifndef DEM_H
 #define DEM_H
 
+/* C++ Standard Libraries */
+#include <cstdlib>
+
+/* GDAL libraries. Check dependencies in README. */
 #include <gdal.h>
 #include <gdal_priv.h>
 #include <gdal_alg.h>
+
+
 
 
 
