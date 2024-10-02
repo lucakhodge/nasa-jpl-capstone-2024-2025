d89fe0d273fda13a7775241d2f8470c3e0248aa0
rywags <rywa2447@colorado.edu>
2024-10-02 00:07:17 -0600

    Method to open DEM

--- dem.h
+++ dem.h
@@ -2,16 +2,27 @@
 #define DEM_H
 
 /* C++ Standard Libraries */
-#include <cstdlib>
+#include <string>
+#include <filesystem>
 
 /* GDAL libraries. Check dependencies in README. */
-#include <gdal.h>
 #include <gdal_priv.h>
-#include <gdal_alg.h>
 
+namespace MARS_PROJECT
+{
+    class DEM
+    {
+    public:
+        DEM(std::string_view ifp, std::string_view ofp); // Constructor
+        ~DEM();                                          // Destructor
 
+        GDALDataset* dem_load();
 
+    private:
+        std::filesystem::path dem_fp; // Original DEM filepath.
+        std::filesystem::path out_fp; // Output directory.
+        GDALDataset *dem_dataset;     // Pointer to the GDAL dataset.
+    };
+};
 
-
-
-#endif
+#endif // DEM_H
