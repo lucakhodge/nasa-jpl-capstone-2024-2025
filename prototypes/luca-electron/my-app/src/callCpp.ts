8813bf4b917fbe13b5134d3542657316a025d1ad
Luca Hodge <lucahodge@gmail.com>
2024-11-19 00:51:37 -0700

    c executable now works in bundling, need to have OS + archetecture specific executables

--- prototypes/luca-electron/my-app/src/callCpp.ts
+++ prototypes/luca-electron/my-app/src/callCpp.ts
@@ -1,15 +1,22 @@
 import { exec } from 'child_process';
-import { ipcMain } from "electron";
+import { app, ipcMain } from "electron";
+import path from 'path';
 import { promisify } from 'util';
 import { CALL_CPP } from "./electronIPC";
 
 const execPromise = promisify(exec)
 
+const getExecutablePath = () => { 
+  const basePath = app.isPackaged ? path.join(process.resourcesPath, 'assets') : path.join(__dirname, '../../assets'); 
+  // Adjust for Vite's structure 
+  return path.join(basePath, 'example'); 
+  };
+
 ipcMain.handle(CALL_CPP, async () => {
   console.log("in call cpp handle")
   try {
     //NOTE: the path is from where you run from
-      const { stdout, stderr } = await execPromise('../cpp-program/example');
+      const { stdout, stderr } = await execPromise(getExecutablePath());
       if (stderr) {
           throw new Error(stderr);
       }
