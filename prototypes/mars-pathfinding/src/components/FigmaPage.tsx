3bd8973f870a6a42b83082db4b2cb29511764c59
Luca Hodge <luho3568@colorado.edu>
2025-02-13 19:20:34 -0700

    pulled in old map

--- prototypes/mars-pathfinding/src/components/FigmaPage.tsx
+++ prototypes/mars-pathfinding/src/components/FigmaPage.tsx
@@ -2,39 +2,44 @@ import { useState } from 'react';
 import { MyButton } from './MyButton'
 import { MyNumberInput } from './MyNumberInput'
 import LoadFileButton from './LoadFileButton';
+import { useSelector } from 'react-redux';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { selectEndCoordinate, selectSlope, selectStartCoordinate, setEndCoordinate, setSlope, setStartCoordinate } from '../store/paramatersSlice';
+import RawThree from './RawThree';
 
 
 
 export const FigmaPage = (props: {}) => {
 
-  const [startCoordinateX, setStartCoordinateX] = useState<number | null>(null);
-  const [startCoordinateY, setStartCoordinateY] = useState<number | null>(null);
-  const [endCoordinateX, setEndCoordinateX] = useState<number | null>(null);
-  const [endCoordinateY, setEndCoordinateY] = useState<number | null>(null);
-  const [slope, setSlope] = useState<number | null>(null);
+  const dispatch = useAppDispatch();
+
+  const startCoordinate = useAppSelector(selectStartCoordinate);
+  const endCoordinate = useAppSelector(selectEndCoordinate);
+  const slope = useAppSelector(selectSlope);
+
   return (
     <div className="grid grid-cols-[1fr_2fr] w-full h-full border-dashed bg-blue-200 absolute">
       <div className='flex flex-col gap-1 bg-gray-600 w-full h-full, p-5'>
         <div className='text-center text-[20px] text-red-500 font-bold'>Mempa</div>
         <div className='flex flex-row'>
           <div className='whitespace-nowrap'>Start Coordinate: (</div>
-          <MyNumberInput value={startCoordinateX} onChange={(val) => {
-            setStartCoordinateX(val)
+          <MyNumberInput value={startCoordinate.x} onChange={(val) => {
+            dispatch(setStartCoordinate({ x: val, y: startCoordinate.y }))
           }}></MyNumberInput>
           <div>,</div>
-          <MyNumberInput value={startCoordinateY} onChange={(val) => {
-            setStartCoordinateY(val)
+          <MyNumberInput value={startCoordinate.y} onChange={(val) => {
+            dispatch(setStartCoordinate({ x: startCoordinate.x, y: val }))
           }}></MyNumberInput>
           <div>)</div>
         </div>
         <div className='flex flex-row'>
           <div className='whitespace-nowrap'>End Coordinate: (</div>
-          <MyNumberInput value={endCoordinateX} onChange={(val) => {
-            setEndCoordinateX(val)
+          <MyNumberInput value={endCoordinate.x} onChange={(val) => {
+            dispatch(setEndCoordinate({ x: val, y: endCoordinate.y }))
           }}></MyNumberInput>
           <div>,</div>
-          <MyNumberInput value={endCoordinateY} onChange={(val) => {
-            setEndCoordinateY(val)
+          <MyNumberInput value={endCoordinate.y} onChange={(val) => {
+            dispatch(setEndCoordinate({ x: endCoordinate.x, y: val }))
           }}></MyNumberInput>
           <div>)</div>
         </div>
@@ -43,7 +48,9 @@ export const FigmaPage = (props: {}) => {
         <div>Number of iterations</div>
         <div className='flex flex-row'>
           <div className='mr-2'>{"Slope tolerance: "}</div>
-          <MyNumberInput value={slope} onChange={(val) => setSlope(val)}></MyNumberInput>
+          <MyNumberInput value={slope} onChange={(val) => {
+            dispatch(setSlope(val))
+          }}></MyNumberInput>
         </div>
         <div className='flex-1' />
         <div className='grid grid-cols-2 gap-5'>
@@ -51,7 +58,9 @@ export const FigmaPage = (props: {}) => {
           <MyButton >Display Optimum Path</MyButton>
         </div>
       </div>
-      <div style={{ width: "100%", height: "100%" }} className='bg-slate-500'>Map</div>
+      <div style={{ width: "100%", height: "100%" }} className='bg-slate-500'>
+        <RawThree></RawThree>
+      </div>
     </div>
   )
 }
