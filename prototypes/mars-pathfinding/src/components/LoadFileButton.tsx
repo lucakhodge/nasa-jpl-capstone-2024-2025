4eb2596adc3188482359da82aa6edfa9bf36bdd8
Adam Carlson <bronadam25@gmail.com>
2025-02-26 10:47:04 -0700

    enhanced UI

--- prototypes/mars-pathfinding/src/components/LoadFileButton.tsx
+++ prototypes/mars-pathfinding/src/components/LoadFileButton.tsx
@@ -4,7 +4,9 @@ export default function LoadFileButton() {
   return (
     <MyButton onClick={() => {
       window.electronIPC.openDEM();
-    }}>
+    }}
+    className="text-green-500"
+    >
       Input DEM file
     </MyButton>
   );
