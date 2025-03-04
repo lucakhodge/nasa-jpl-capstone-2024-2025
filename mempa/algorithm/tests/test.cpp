f8d191e17c98236111959d97b821025b3708dd2e
Kevin Buhler <buhlerkw@gmail.com>
2025-03-04 16:31:16 -0700

    one main

--- mempa/algorithm/tests/test.cpp
+++ mempa/algorithm/tests/test.cpp
@@ -1,12 +1,12 @@
-#include <gtest/gtest.h>
+// #include <gtest/gtest.h>
 
-int add(int a, int b) { return a + b; }
+// int add(int a, int b) { return a + b; }
 
-TEST(AddTest, Basic) {
-    EXPECT_EQ(add(2, 3), 5);
-}
+// TEST(AddTest, Basic) {
+//     EXPECT_EQ(add(2, 3), 5);
+// }
 
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+// int main(int argc, char **argv) {
+//     ::testing::InitGoogleTest(&argc, argv);
+//     return RUN_ALL_TESTS();
+// }
