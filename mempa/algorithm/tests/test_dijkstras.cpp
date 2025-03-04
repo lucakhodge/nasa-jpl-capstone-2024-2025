7b3aa336e51158f8fe701e8b8f7d30f9684a2258
Kevin Buhler <buhlerkw@gmail.com>
2025-03-04 16:25:13 -0700

    djik path

--- mempa/algorithm/tests/test_dijkstras.cpp
+++ mempa/algorithm/tests/test_dijkstras.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include "dijkstras.h"
+#include "../src/search_algorithms/dijkstras.h"
 
 // Test fixture for Dijkstra's tests
 class DijkstrasTest : public ::testing::Test {
