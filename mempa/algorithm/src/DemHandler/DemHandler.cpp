af9feaee5e65008bda6d83f8b9b554d1d2ee5d8c
rywags <rywa2447@colorado.edu>
2025-03-05 02:10:53 -0700

    New DemHandler. TODO: Reimplement into Simulator.

---
+++ mempa/algorithm/src/DemHandler/DemHandler.cpp
@@ -0,0 +1,170 @@
+#include "DemHandler.h"
+#include <algorithm>
+#include <cmath>
+namespace mempa
+{
+    /**
+     * @brief Construct a new Dem Handler:: Dem Handler object
+     *
+     * @param pszFilename Filepath to the DEM raster to be read.
+     * @param radius The value used for the general size of the output chunk of elevation data.
+     *
+     * @note For more information on GDAL Raster API, see: https://gdal.org/en/stable/tutorials/raster_api_tut.html
+     */
+    DemHandler::DemHandler(const char *const pszFilename, const int radius)
+        : pszFilename(pszFilename), radius(radius)
+    {
+        if (radius <= 0)
+        {
+            throw std::runtime_error("Cannot read chunk of non-positive radius.");
+        }
+        GDALAllRegister();
+        poDataset = GDALDatasetUniquePtr(GDALDataset::FromHandle(GDALOpen(pszFilename, GA_ReadOnly)));
+        if (!poDataset)
+        {
+            throw std::runtime_error("Failure to create GDAL dataset.");
+        }
+        poBand = poDataset->GetRasterBand(ELEVATION_BAND);
+        if (!poBand)
+        {
+            throw std::runtime_error("Failure to get raster band.");
+        }
+        if (poDataset->GetGeoTransform(adfGeoTransform) != CE_None)
+        {
+            throw std::runtime_error("Failure to get geotransform.");
+        }
+    }
+
+    /**
+     * @brief Creates a square chunk from a single coordinate point. The input coordinate will be the centerpoint of the chunk.
+     *
+     * @param imgCoordinate A pair of integer image coordinates.
+     * @return std::vector<std::vector<float>>
+     */
+    std::vector<std::vector<float>> DemHandler::readSquareChunk(const std::pair<int, int> &imgCoordinate) const
+    {
+        const int xCenter = imgCoordinate.first;
+        const int yCenter = imgCoordinate.second;
+        const int xOff = std::max(0, xCenter - radius);
+        const int yOff = std::max(0, yCenter - radius);
+        const int xEnd = std::min(poBand->GetXSize(), xCenter + radius + 1);
+        const int yEnd = std::min(poBand->GetYSize(), yCenter + radius + 1);
+        const int xSize = xEnd - xOff;
+        const int ySize = yEnd - yOff;
+        float *pafScanline = reinterpret_cast<float *>(CPLMalloc(sizeof(float) * xSize * ySize));
+        if (!pafScanline)
+        {
+            throw std::runtime_error("Failed to allocate memory.");
+        }
+        if (poBand->RasterIO(GF_Read, xOff, yOff, xSize, ySize, pafScanline, xSize, ySize, GDT_Float32, sizeof(float), (sizeof(float) * xSize)) != CE_None)
+        {
+            CPLFree(pafScanline);
+            throw std::runtime_error("Failure to read raster values.");
+        }
+        std::vector<std::vector<float>> rasterVector(xSize, std::vector<float>(ySize));
+        for (int xVec = 0; xVec < xSize; ++xVec)
+        {
+            for (int yVec = 0; yVec < ySize; ++yVec)
+            {
+                rasterVector[yVec][xVec] = pafScanline[xSize * yVec + xVec];
+            }
+        }
+        CPLFree(pafScanline);
+        return rasterVector;
+    }
+
+    /**
+     * @brief Creates a circular chunk from a single coordinate point. The input coordinate will be the centerpoint of the chunk.
+     *
+     * @details The corner values of the initial square will be overwritten with NaN.
+     *
+     * @param imgCoordinate A pair of integer image coordinates.
+     * @return std::vector<std::vector<float>>
+     */
+    std::vector<std::vector<float>> DemHandler::readCircleChunk(const std::pair<int, int> &imgCoordinate) const
+    {
+        std::vector<std::vector<float>> rasterVector = readSquareChunk(imgCoordinate);
+        if (rasterVector.empty())
+        {
+            throw std::runtime_error("Cannot read empty chunk.");
+        }
+        const int xVec = rasterVector.size();
+        const int yVec = rasterVector.front().size();
+        const int xCenter = xVec / 2;
+        const int yCenter = yVec / 2;
+        for (int row = 0; row < xVec; ++row)
+        {
+            for (int col = 0; col < yVec; ++col)
+            {
+                int xDistance = (col - xCenter) * (col - xCenter);
+                int yDistance = (row - yCenter) * (row - yCenter);
+                if (xDistance + yDistance > radius * radius)
+                {
+                    rasterVector[row][col] = std::nanf("");
+                }
+            }
+        }
+        return rasterVector;
+    }
+
+    /**
+     * @brief Creates a rectangular chunk from a pair of coordinate points. Values sorrounding both coordinates as well as every value in between them will make up the chunk.
+     *
+     * @details The coordinates will not necessarily be the center points of the chunk, but should be around opposing corners buffered by the radius.
+     *
+     * @param geoCoordinates A pair of pairs of integer coordinates.
+     * @return std::vector<std::vector<float>>
+     */
+    std::vector<std::vector<float>> DemHandler::readRectangleChunk(const std::pair<std::pair<int, int>, std::pair<int, int>> &geoCoordinates) const
+    {
+        const std::pair<int, int> imgCoordinate1 = transformCoordinates(geoCoordinates.first.first, geoCoordinates.first.second);
+        const std::pair<int, int> imgCoordinate2 = transformCoordinates(geoCoordinates.second.first, geoCoordinates.second.second);
+        const int xCenter1 = imgCoordinate1.first;
+        const int yCenter1 = imgCoordinate1.second;
+        const int xCenter2 = imgCoordinate2.first;
+        const int yCenter2 = imgCoordinate2.second;
+        const int xOff = std::max(0, std::min(xCenter1 - radius, xCenter2 - radius));
+        const int yOff = std::max(0, std::min(yCenter1 - radius, yCenter2 - radius));
+        const int xEnd = std::min(poBand->GetXSize(), std::max(xCenter1 + radius + 1, xCenter2 + radius + 1));
+        const int yEnd = std::min(poBand->GetYSize(), std::max(yCenter1 + radius + 1, yCenter2 + radius + 1));
+        const int xSize = xEnd - xOff;
+        const int ySize = yEnd - yOff;
+        float *pafScanline = reinterpret_cast<float *>(CPLMalloc(sizeof(float) * xSize * ySize));
+        if (!pafScanline)
+        {
+            throw std::runtime_error("Failed to allocate memory.");
+        }
+        if (poBand->RasterIO(GF_Read, xOff, yOff, xSize, ySize, pafScanline, xSize, ySize, GDT_Float32, sizeof(float), (sizeof(float) * xSize)) != CE_None)
+        {
+            CPLFree(pafScanline);
+            throw std::runtime_error("Failure to read raster values.");
+        }
+        std::vector<std::vector<float>> rasterVector(xSize, std::vector<float>(ySize));
+        for (int xVec = 0; xVec < xSize; ++xVec)
+        {
+            for (int yVec = 0; yVec < ySize; ++yVec)
+            {
+                rasterVector[xVec][yVec] = pafScanline[xSize * yVec + xVec];
+            }
+        }
+        CPLFree(pafScanline);
+        return rasterVector;
+    }
+
+    /**
+     * @brief Transform a latitide, longitude (x, y) pair of (double) geographical coordinates into the corresponding pair of (integer) image pixel (x, y) coordiantes.
+     *
+     * @param xGeoCoordinate The longitude value.
+     * @param yGeoCoordinate The latitude value.
+     *
+     * @return std::pair<int, int>
+     *
+     * @note For more information on Geotransform, see: https://gdal.org/en/stable/tutorials/geotransforms_tut.html
+     */
+    std::pair<int, int> DemHandler::transformCoordinates(const double xGeoCoordinate, const double yGeoCoordinate) const
+    {
+        const int xPixelCoordinate = static_cast<int>((xGeoCoordinate - adfGeoTransform[0]) / adfGeoTransform[1]);
+        const int yPixelCoordinate = static_cast<int>((yGeoCoordinate - adfGeoTransform[3]) / adfGeoTransform[5]);
+        return std::make_pair(xPixelCoordinate, yPixelCoordinate);
+    }
+}
---
+++ mempa/algorithm/src/DemHandler/DemHandler.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <vector>
+#include <utility>
+#include <gdal_priv.h>
+/**
+ * @brief Namespace for CU Boulder Mars Elevation Model Pathfinding Algorithm.
+ *
+ */
+namespace mempa
+{
+    /**
+     * @brief Class to hold DEM interactions for CU Boulder MEMPA simulation.
+     * 
+     */
+    class DemHandler
+    {
+    public:
+        explicit DemHandler(const char *pszFilename, int radius);
+        std::vector<std::vector<float>> readSquareChunk(const std::pair<int, int> &imgCoordinate) const;
+        std::vector<std::vector<float>> readCircleChunk(const std::pair<int, int> &imgCoordinate) const;
+        std::vector<std::vector<float>> readRectangleChunk(const std::pair<std::pair<int, int>, std::pair<int, int>> &geoCoordinates) const;
+        std::pair<int, int> transformCoordinates(double xGeoCoordinate, double yGeoCoordinate) const;
+
+    private:
+        const char *const pszFilename;
+        const int radius;
+        GDALDatasetUniquePtr poDataset;
+        GDALRasterBand *poBand;
+        static constexpr int ELEVATION_BAND = 1;
+        static constexpr int GEOTRANSFORM_SIZE = 6;
+        double adfGeoTransform[GEOTRANSFORM_SIZE];
+    };
+}
