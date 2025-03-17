3d53a4582aacb957f5a79e0f90e4a94349f2bbae
Kevin Buhler <buhlerkw@gmail.com>
2025-03-17 09:56:25 -0600

    SearchAlgorithm::get_step

--- mempa/algorithm/src/search_algorithms/SearchAlgorithm.cpp
+++ mempa/algorithm/src/search_algorithms/SearchAlgorithm.cpp
@@ -1,4 +1,4 @@
-#include"SearchAlgorithm.h"
+#include "SearchAlgorithm.h"
 
 void SearchAlgorithm::setUpAlgo(std::vector<std::vector<float> > &heightmap, std::pair<int, int> startPoint, std::pair<int, int> endPoint, float maxSlope, float pixelSize) 
 {
@@ -7,4 +7,8 @@ void SearchAlgorithm::setUpAlgo(std::vector<std::vector<float> > &heightmap, std
     _endPoint = endPoint;
     _maxSlope = maxSlope;
     _pixelSize = pixelSize;
+}
+
+std::pair<int, int> SearchAlgorithm::get_step() {
+    return {-1, -1};
 }
