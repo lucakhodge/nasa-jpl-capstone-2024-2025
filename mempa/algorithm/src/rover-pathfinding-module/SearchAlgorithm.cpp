d83f02f99a5e0084f6d4788f3962ed8271f3ff66
OscarMikus <osmi3783@colorado.edu>
2025-04-17 15:35:59 -0600

    Added a doxygen documentation for SearchAlgorithm.cpp

--- mempa/algorithm/src/rover-pathfinding-module/SearchAlgorithm.cpp
+++ mempa/algorithm/src/rover-pathfinding-module/SearchAlgorithm.cpp
@@ -2,6 +2,16 @@
 
 SearchAlgorithm::SearchAlgorithm() noexcept {}
 
+/**
+ * @brief non-default way to set up internal parameters to be used with a SearchAlgorithm subclass
+ * 
+ * @param heightmap contains the height values to be used for naviagtion, Usualy a chunk of a larger heightmap
+ * @param chunkLocation 0,0 in the passed heightmap is this value in the whole larger heightmap (global context)
+ * @param startPoint the start point for navigation in the whole larger heightmap (global context)
+ * @param endPoint the end point for nagivation in the whole larger heightmap (global context)
+ * @param maxSlope the maximum slope that is allowed to be navigated over
+ * @param pixelSize the size (in meters) of the resolution of the heightmap
+ */
 void SearchAlgorithm::setUpAlgo(std::vector<std::vector<float>> &heightmap,
                                 std::pair<int, int> chunkLocation,
                                 std::pair<int, int> startPoint,
